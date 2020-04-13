b5b2d18b5df76e18f704972c4cd0c92dc11b7a44
SofiaSSR <unitargetofsofi@gmail.com>
2020-04-13 18:57:25 -0500

    tercera entrega

--- proyectoUwU.js
+++ proyectoUwU.js
@@ -5,15 +5,23 @@ n = Number(n);
 return(n)
 
 }
+function texto(c1,c2,c3,texto){
+  stroke(c1,c2,c3);
+  textSize(32);
+  text(texto,145,985);
+  noStroke();
+  noFill();
+}
+var fondo = 90;
 function setup() {
   var canvas = createCanvas(1000,1000);
   canvas.parent("canvas")
-  background(3);
+  background(fondo,fondo,fondo);
   /* var button = createButton("stop");
   button.mousePressed(pausa(1)); */
 }
-var guardado ;
-var n = 20;// numero de ejes
+var guardado ;//guarda el punto anterior 
+var n = 4;// numero de ejes
 var t = 0;//el punto inicial
 /* function pausa(n){
   
@@ -23,17 +31,13 @@ var t = 0;//el punto inicial
     aumento =0.005;
   }
   return(aumento) } */
- var aumento =0.005;//variacion entre los valores de 
-  var grande =[]
-  var color1 = random(250);
-  var color2= random (250);
-function draw() {
-    if (n ==0 ) {
-      console.allert("eso no se puede");
-    }else{
-      stroke(3);
-      textSize(32);
-      text(guardado,145,985);
+ var aumento =0.0065;//variacion entre los valores de t
+  var grande =[] ;//guarda los puntos para hacer la curva entera
+  var colorin1 = random(200);//parametro 1 del rgb
+  var colorin2= random(240);//parametro 2 del rgb
+  function draw() {
+    strokeWeight((n/(n/5))-1.5);
+      texto(fondo,fondo,fondo,guardado);
    var equation = exp(cos(t)) - 2*cos(n*t) - pow(sin(t/12),5);
   var py1 = -cos(t+aumento)*equation*100 + width/2;
    var px1 = -sin(t+aumento)*equation*100 +height/2;
@@ -42,22 +46,22 @@ function draw() {
    var py2 = -cos(t)*equation*100 + width/2;
    var px2 = -sin(t)*equation*100 +height/2;
    var distancia = sqrt(((py1-py2)**2)+((px1-px2)**2));
-    console.log(distancia);
-   var chiquito = [px1,py1];
+    if(distancia>0.03 || px1 == 500.000 ){
+      var chiquito = [px1,py1];
     grande.push(chiquito); 
-   if(distancia>0.04){
-   stroke(0,color1,color2);
-   }else{
-    stroke(color1 = random(250),0,color2= random(250));
+   stroke(colorin1,0,colorin2);
+  }else{
+    stroke(colorin1,0,colorin2);
+     beginShape();
+     for (i=0;i<grande.length;i++){
+     curveVertex(grande[i][0],grande[i][1]);
+     }
+     endShape();
+     grande = [];
+    stroke(colorin1 = random(200),colorin2= random(200),0);
    }
-  strokeWeight((n/(n/5))-1.5);
   point(px1,py1);
-  stroke("white");
-  fill("white");
-  textSize(32);
-  text(legend,145,985);
-  noStroke();
+  texto(0,39,39,legend);
   //10**-3 + (10**-2)*(n/2)
   t+=aumento;
   }
-}
